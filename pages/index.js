2e77a5030df36c4ae16adb3a0a48fb2d2950353f
DKGara14 <142203278+DKGara14@users.noreply.github.com>
2024-12-16 17:16:40 +0000

    desafio terminado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h1>Hola</h1>
+    return <h1>Hola amor, se que no estamos bien, no estamos al 100%. Pero en mi curso pidieron para enviar este link a alguien especial, entonces te lo envio a ti, solo para hacerte recordar de que independiente de como esten las cosas, mi amor o sentimientos no cambian, te amo mujercita, buena suerte en esta semana dura para ti, estare contigo en todos los aspectos...
+    </h1>
 }
 
 export default Home;
